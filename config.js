8452e3b8ba2d52ee81c672c6a6f547a3b7b8fa2e
pr-coviam <partha.roy@coviam.com>
2016-12-10 13:03:45 +0530

    first code commit

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAA9xuy5R4:APA91bGRBqHn8cn_pik9vVVtX5kjRKJXlzbeIzCIY1fIsBOJ65Y2KfI0xn-hVllyUSO3quSwHioUZWzXEliWdVF8LfvE0UstvrSe5c2WtBjQA7kJgmGpGtqYTSHfZnxfvbFmvBzdp97aCYt-fDLkyb_3F9X63I0RZA'
+};
