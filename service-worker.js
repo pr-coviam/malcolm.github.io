932118dd056c002023b6bbddf388c98f669311bb
pr-coviam <partha.roy@coviam.com>
2016-12-10 17:03:50 +0530

    service worker changed for custom link redirection

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 'use strict';
-var website_link = "https://pr-coviam.github.io/malcolmPush/";
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
   console.log(event);
@@ -8,19 +7,36 @@ self.addEventListener('push', function(event) {
   var body = 'We have received a push message.';
   var icon = 'http://icons.iconarchive.com/icons/elegantthemes/beautiful-flat/128/anchor-icon.png';
   var tag = 'simple-push-demo-notification-tag';
-
+  var data = {
+    doge: {
+        wow: 'such amaze notification data'
+    },
+    url: "https://pr-coviam.github.io/malcolmPush/"
+  };
   event.waitUntil(
     self.registration.showNotification(title, {
       body: body,
       icon: icon,
-      tag: tag
+      tag: tag,
+      data:data
     })
   );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  return clients.openWindow("https://pr-coviam.github.io/malcolmPush/");
-  event.notification.close();
+  if (Notification.prototype.hasOwnProperty('data')) {
+    console.log('Using Data');
+    var url = event.notification.data.url;
+    event.waitUntil(clients.openWindow(url));
+  } else {
+    event.waitUntil(getIdb().get(KEY_VALUE_STORE_NAME,
+event.notification.tag).then(function(url) {
+      // At the moment you cannot open third party URL's, a simple trick
+      // is to redirect to the desired URL from a URL on your domain
+      var redirectUrl = '/redirect.html?redirect=' +
+        url;
+      return clients.openWindow(redirectUrl);
+    }));
+  }
 
 });
